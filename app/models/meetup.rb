1a10c31c8931a33d4dcf644a233fb29b635d6a17
anna bozio <anna.bozio@gmail.com>
2021-08-23 17:30:00 +0200

    all relations and validates done

--- app/models/meetup.rb
+++ app/models/meetup.rb
@@ -1,4 +1,7 @@
 class Meetup < ApplicationRecord
   belongs_to :user
   belongs_to :offer
+  validates :name, presence: true
+  validates :date, presence: true
+  validates :description, presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :offer
+  validates :rating, presence: true, inclusion: { in: 1..5 }
+  validates :comment, presence: true
 end
