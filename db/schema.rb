4c503aa5e312f7492d2c3a986e7ad3a1f71c5314
Anne Barois <barois.anne@orange.fr>
2021-08-24 15:10:15 +0200

    new in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 2021_08_24_121258) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -66,7 +68,6 @@
     t.float "latitude"
     t.float "longitude"
     t.string "photo"
-
     t.index ["user_id"], name: "index_offers_on_user_id"
   end
 
