4724914ead91aa8c027dd2627e9095adac2d80bc
Estelle <estelle.cirilo@gmail.com>
2021-08-24 12:39:06 +0200

    view index bookmarks generate

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 
   resources :offers
 
-  resources :bookings, only: [:new, :create]
+  resources :bookings, only: [:new, :create, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
