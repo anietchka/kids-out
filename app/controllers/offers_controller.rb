2b3db6b959d64161994b2dc2d23ec25b7ddbc211
Mickael Gaspar <migatsar@gmail.com>
2021-08-24 13:37:34 +0200

    Implemented show action for Offer

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,2 +1,8 @@
 class OffersController < ApplicationController
+  skip_before_action :authenticate_user!, only: :show
+
+  def show
+    @offer = Offer.find(params[:id])
+    authorize @offer
+  end
 end
