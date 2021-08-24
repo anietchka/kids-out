152c1011f5889a31658e658dc8631f152ad641b9
anna bozio <anna.bozio@gmail.com>
2021-08-24 15:58:46 +0200

    reviews policies done

--- app/policies/review_policy.rb
+++ app/policies/review_policy.rb
@@ -2,6 +2,25 @@ class ReviewPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
       scope.all
+      Scope.where(user: user)
     end
   end
+
+   def index?
+    true
+  end
+
+
+  def create?
+    user
+  end
+
+  def new?
+    create?
+  end
+
+  def destroy?
+    record.user == user
+  end
+
 end
