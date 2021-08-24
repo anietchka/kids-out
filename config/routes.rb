d41d3da6bfac75c9c56f97f2e21824d6ecefd2e2
Mickael Gaspar <migatsar@gmail.com>
2021-08-24 17:23:47 +0200

    Created routes and view for offers#new

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-
-  resources :offers, only: [:index, :show] do
+  resources :offers, only: [:new, :create, :index, :show] do
     resources :bookmarks, only: [:new, :create, :index, :destroy]
     resources :reviews, only: [:index, :new, :create, :destroy]
   end
 
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
 end
