d64803b030f21308809945975c9d95af8105edaf
Anne Barois <barois.anne@orange.fr>
2021-08-24 13:18:54 +0200

    Create in Offer controler : new, create, index. Add gem geocoder. View offer index with map box

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,2 +1,32 @@
 class OffersController < ApplicationController
+  def new
+    @offer = Offer.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @offer = Offer.new(offers_params)
+    @offer.user = @user
+    @offer.save
+    redirect_to root_path
+  end
+
+  def index
+    @offers = Offer.all
+
+    # the `geocoded` scope filters only offers with coordinates (latitude & longitude)
+    @markers = @offers.geocoded.map do |offer|
+      {
+        lat: offer.latitude,
+        lng: offer.longitude
+      }
+    end
+  end
+
+  private
+
+  def offers_params
+    params.require(:offer).permit(:name, :address, :start_date, :end_date, :url, :permanent, :description, :min_age, :max_age, :schedule, :longitude, :latitude, :user_id )
+  end
+
 end
