0c8037a4933831552af73e5f023b9b38e0d32147
Estelle <estelle.cirilo@gmail.com>
2021-08-24 14:44:32 +0200

    view bookmark ok

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :offers
+  resources :offers do
+    resources :bookmarks, only: [:new, :create, :index]
+  end
 
-  resources :bookmarks, only: [:new, :create, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
