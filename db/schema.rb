e78a972227c27648b7ea3eed22687dad0aed9696
Anne Barois <barois.anne@orange.fr>
2021-08-31 11:15:07 +0200

    Beging word

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2021_08_30_152830) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -97,9 +96,9 @@ ActiveRecord::Schema.define(version: 2021_08_30_152830) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "photo"
     t.float "latitude"
     t.float "longitude"
+    t.string "photo"
     t.string "theme"
     t.index ["user_id"], name: "index_offers_on_user_id"
   end
