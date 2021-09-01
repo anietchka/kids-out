9ffdaac63c9a40f6cf8f6f51d1de06a95a7f512a
Estelle <estelle.cirilo@gmail.com>
2021-09-01 16:31:05 +0200

    fix sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :nickname, presence: true, uniqueness: true
   has_many :bookmarks, dependent: :destroy
