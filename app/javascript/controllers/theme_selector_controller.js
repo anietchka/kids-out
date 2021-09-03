1d110ddf8ceb512a94d4092540c7f07e5bca1948
anna bozio <anna.bozio@gmail.com>
2021-09-03 11:27:15 +0200

    zoom map stimulus theme color text inactive navbar

--- app/javascript/controllers/theme_selector_controller.js
+++ app/javascript/controllers/theme_selector_controller.js
@@ -1,17 +1,21 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["theme1", "theme2", "input"]
+  static targets = ["border1", "text1", "border2", "text2", "input"]
 
   change1() {
     this.inputTarget.value = "interieure";
-    this.theme1Target.classList.add("active")
-    this.theme2Target.classList.remove("active")
+    this.text1Target.classList.add("active");
+    this.border1Target.classList.add("active");
+    this.text2Target.classList.remove("active");
+    this.border2Target.classList.remove("active");
   }
 
   change2() {
     this.inputTarget.value = "exterieure";
-    this.theme2Target.classList.add("active")
-    this.theme1Target.classList.remove("active")
+    this.text2Target.classList.add("active");
+    this.border2Target.classList.add("active");
+    this.text1Target.classList.remove("active");
+    this.border1Target.classList.remove("active");
   }
 }
